6756f642c5326ff8e9e87c3fa5b7ade3b572dee3
Monirah90 <113800067+Monirah90@users.noreply.github.com>
2023-01-12 17:36:05 +0300

    ADD
    NEW

---
+++ ImageColorPicker-2/Shared/ImageColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageColorPickerApp.swift
+//  Shared
+//
+// 
+//
+
+import SwiftUI
+
+@main
+struct ImageColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageColorPicker-2/Shared/View/Home.swift
@@ -0,0 +1,43 @@
+//
+//  Home.swift
+//  ImageColorPicker (iOS)
+//
+
+//
+
+import SwiftUI
+
+struct Home: View {
+    // MARK: Image Color Picker Values
+    @State var showPicker: Bool = false
+    @State var selectedColor: Color = .white
+    
+    
+    var body: some View {
+        
+        ZStack{
+            
+            Rectangle()
+                .fill(selectedColor)
+                .ignoresSafeArea()
+            
+            // MARK: Picker Button
+            Button {
+                showPicker.toggle()
+            } label: {
+             
+                Text("Show Image Color Picker")
+                    .foregroundColor(selectedColor.isDarkColor ? .white : .black)
+            }
+            
+        }
+        // MARK: Calling Modifier
+        .imageColorPicker(showPicker: $showPicker, color: $selectedColor)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
